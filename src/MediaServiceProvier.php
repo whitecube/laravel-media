cc9e0374a6e8d761387e2e3bdcb5e3edffac34d4
Toon Van den Bos <toon@whitecube.be>
2026-05-20 19:04:12 +0200

    Added base composer package config

---
+++ src/MediaServiceProvier.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Whitecube\Media;
+
+use Illuminate\Support\ServiceProvider;
+
+class MediaServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
